27310b3ab76c5aaa324420e4bf555fae558a00e6
Asayu123 <Asayu123@users.noreply.github.com>
2017-09-19 20:46:37 +0900

    Added Sample Test Code for Python Subtest Examples.

---
+++ src/unit_test/parameterized_test/test_with_subtest.py
@@ -0,0 +1,28 @@
+from unittest import TestCase, main
+from src.unit_test.parameterized_test.test_target import rock_paper_scissors
+
+
+class TestRockPaperScissors(TestCase):
+    """ This test case tests rock_paper_scissors method """
+
+    def test_with_valid_params(self):
+
+        test_params = [
+            ("Rock", "Rock", "Even"),
+            ("Rock", "Paper", "Player2"),
+            ("Rock", "Scissor", "Player1"),
+            ("Paper", "Rock", "Player1"),
+            ("Paper", "Paper", "Even"),
+            ("Paper", "Scissor", "Player2"),
+            ("Scissor", "Rock", "Player2"),
+            ("Scissor", "Paper", "Player1"),
+            ("Scissor", "Scissor", "Even"),
+        ]
+
+        for p1_shape, p2_shape, expected_result in test_params:
+            with self.subTest(p1_shape=p1_shape, p2_shape=p2_shape):
+                self.assertEqual(rock_paper_scissors(p1_shape=p1_shape, p2_shape=p2_shape), expected_result)
+
+
+if __name__ == '__main__':
+    main()
