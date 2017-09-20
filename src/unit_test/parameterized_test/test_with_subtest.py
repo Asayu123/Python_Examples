d1777a2f664fd4e8e94f111027f71ae9306114e1
Asayu123 <Asayu123@users.noreply.github.com>
2017-09-20 12:33:46 +0900

    Add comments and changes variable naming on Subtest examples.

--- src/unit_test/parameterized_test/test_with_subtest.py
+++ src/unit_test/parameterized_test/test_with_subtest.py
@@ -6,20 +6,24 @@ class TestRockPaperScissors(TestCase):
     """ This test case tests rock_paper_scissors method """
 
     def test_with_valid_params(self):
+        """
+        With SubTest, All patterns will be tested even if there are some failures.
+        Consequently, we can test all patterns every time.
+        """
 
-        test_params = [
-            ("Rock", "Rock", "Even"),
-            ("Rock", "Paper", "Player2"),
-            ("Rock", "Scissor", "Player1"),
-            ("Paper", "Rock", "Player1"),
-            ("Paper", "Paper", "Even"),
-            ("Paper", "Scissor", "Player2"),
-            ("Scissor", "Rock", "Player2"),
-            ("Scissor", "Paper", "Player1"),
-            ("Scissor", "Scissor", "Even"),
+        test_patterns = [
+            ("Rock", "Rock", "Even"),  # Will pass.
+            ("Rock", "Paper", "Player2"),  # Will pass.
+            ("Rock", "Scissor", "Player1"),  # Will fail by bugs, and the test result shows test parameters as well.
+            ("Paper", "Rock", "Player1"),  # Will pass.
+            ("Paper", "Paper", "Even"),  # Will pass.
+            ("Paper", "Scissor", "Player2"),  # Will fail by bugs, and the test result shows test parameters as well.
+            ("Scissor", "Rock", "Player2"),  # Will pass.
+            ("Scissor", "Paper", "Player1"),  # Will pass.
+            ("Scissor", "Scissor", "Even"),  # Will pass.
         ]
 
-        for p1_shape, p2_shape, expected_result in test_params:
+        for p1_shape, p2_shape, expected_result in test_patterns:
             with self.subTest(p1_shape=p1_shape, p2_shape=p2_shape):
                 self.assertEqual(rock_paper_scissors(p1_shape=p1_shape, p2_shape=p2_shape), expected_result)
 
--- src/unit_test/parameterized_test/test_without_subtest.py
+++ src/unit_test/parameterized_test/test_without_subtest.py
@@ -6,20 +6,24 @@ class TestRockPaperScissors(TestCase):
     """ This test case tests rock_paper_scissors method """
 
     def test_with_valid_params(self):
+        """
+        Without SubTest, Test will exit on first failure.
+        Consequently, we can not test all patterns.
+        """
 
-        test_params = [
-            ("Rock", "Rock", "Even"),
-            ("Rock", "Paper", "Player2"),
-            ("Rock", "Scissor", "Player1"),
-            ("Paper", "Rock", "Player1"),
+        test_patterns = [
+            ("Rock", "Rock", "Even"),  # Will pass.
+            ("Rock", "Paper", "Player2"),  # Will pass.
+            ("Rock", "Scissor", "Player1"),  # The Unit test will fail with this parameter. Even worse, we can't know that failed test parameters from the test result.
+            ("Paper", "Rock", "Player1"),  # Following parameters are not tested because the test exits on the first failure.
             ("Paper", "Paper", "Even"),
-            ("Paper", "Scissor", "Player2"),
+            ("Paper", "Scissor", "Player2"),  # This pattern should fail by bugs, but won't be detected thanks to above failure.
             ("Scissor", "Rock", "Player2"),
             ("Scissor", "Paper", "Player1"),
             ("Scissor", "Scissor", "Even"),
         ]
 
-        for p1_shape, p2_shape, expected_result in test_params:
+        for p1_shape, p2_shape, expected_result in test_patterns:
             self.assertEqual(rock_paper_scissors(p1_shape=p1_shape, p2_shape=p2_shape), expected_result)
 
 
