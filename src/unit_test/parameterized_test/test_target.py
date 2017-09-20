20c67b475250af776c536b912c7143548dbe1063
Asayu123 <Asayu123@users.noreply.github.com>
2017-09-20 11:57:55 +0900

    Added Intentional Bug to Python Subtest Examples.

--- src/unit_test/parameterized_test/test_target.py
+++ src/unit_test/parameterized_test/test_target.py
@@ -25,10 +25,10 @@ def rock_paper_scissors(p1_shape, p2_shape):
     _win_loss_judgement_table = {
         ("Rock", "Rock"): _EVEN,
         ("Rock", "Paper"): _PLAYER_2,
-        ("Rock", "Scissor"): _PLAYER_1,
+        ("Rock", "Scissor"): _PLAYER_2,  # Bug! Correct result is _PLAYER1
         ("Paper", "Rock"): _PLAYER_1,
         ("Paper", "Paper"): _EVEN,
-        ("Paper", "Scissor"): _PLAYER_2,
+        ("Paper", "Scissor"): _PLAYER_1,  # Bug! Correct result is _PLAYER2
         ("Scissor", "Rock"): _PLAYER_2,
         ("Scissor", "Paper"): _PLAYER_1,
         ("Scissor", "Scissor"): _EVEN,
